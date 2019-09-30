01e14e79abda382faddd8032de5b6bfe655914e0
Ize Imai <izeimai@gmail.com>
2019-09-29 20:27:00 -0400

    Added navbar with logo, moved login page columns, some margins

--- plse-app/src/components/InputForm/index.js
+++ plse-app/src/components/InputForm/index.js
@@ -1,98 +1,121 @@
 import React, { Component } from 'react';
 // import {withRouter} from 'react-router-dom';
 import "./style.css";
-import { Button, Form, Col, Row } from 'react-bootstrap';
+import { Button, Form, Navbar, Col, Row } from 'react-bootstrap';
 
 class InputForm extends Component {
   render() {
-    return (
+    return <div className="text-center">
       <Row>
+        <Col>
+          <Navbar bg="light">
+            <Navbar.Brand href="#home">
+              <img
+                src="http://plsephilly.org/wp-content/uploads/2014/11/PLSE_logotype_320.png"
+                width="90"
+                height="30"
+                className="d-inline-block align-top"
+                alt="PLSE logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                Signed in as: <a href="#login">John Doe</a>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+      <Row style={{margin:`80px`}}>
+
         <Col></Col>
-        <Col md={6}>  
-        
-      <Form>
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Col sm={3}>
-          <Form.Label>
-            Client's Name
+
+        <Col md={6}>
+
+          <Form>
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Col sm={3}>
+                <Form.Label>
+                  Client's Name
           </Form.Label>
-          </Col>
-          <Col md={{ span: 3 }}>
-            <Form.Control defaultValue="First Name" />
-          </Col>
-          <Col sm="5">
-            <Form.Control defaultValue="Last Name" />
-          </Col>
+              </Col>
+              <Col md={{ span: 3 }}>
+                <Form.Control placeholder="First Name" />
+              </Col>
+              <Col sm="5">
+                <Form.Control placeholder="Last Name" />
+              </Col>
 
-        </Form.Group>
+            </Form.Group>
 
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Col sm={3}>
-          <Form.Label>
-            Date of Birth
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Col sm={3}>
+                <Form.Label>
+                  Date of Birth
           </Form.Label>
-          </Col>
-          <Col md={{ span: 3 }}>
-            <Form.Control defaultValue="mm/dd/yyyy" />
-          </Col>
-        </Form.Group>
+              </Col>
+              <Col md={{ span: 3 }}>
+                <Form.Control placeholder="mm/dd/yyyy" />
+              </Col>
+            </Form.Group>
 
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Col sm={3}>
-          <Form.Label>
-            Address
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Col sm={3}>
+                <Form.Label>
+                  Address
           </Form.Label>
-          </Col>
-          <Col sm="7">
-            <Form.Control defaultValue="Street Name, Number" />
-          </Col>
-        </Form.Group>
+              </Col>
+              <Col sm="7">
+                <Form.Control placeholder="Street Name, Number" />
+              </Col>
+            </Form.Group>
 
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Col sm={3}>
-          <Form.Label>     
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Col sm={3}>
+                <Form.Label>
+                </Form.Label>
+              </Col>
+              <Col sm={2}>
+                <Form.Control placeholder="City" />
+              </Col>
+              <Col sm={2}>
+                <Form.Control placeholder="State" />
+              </Col>
+              <Col sm={2}>
+                <Form.Control placeholder="Zip" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Col sm={3}>
+                <Form.Label>
+                  Social Security
           </Form.Label>
-          </Col>
-          <Col sm={2}>
-            <Form.Control defaultValue="City" />
-          </Col>
-          <Col sm={2}>
-            <Form.Control defaultValue="State" />
-          </Col>
-          <Col sm={2}>
-            <Form.Control defaultValue="Zip" />
-          </Col>
-        </Form.Group>
+              </Col>
+              <Col sm="4">
+                <Form.Control placeholder="###-##-####" />
+              </Col>
+            </Form.Group>
 
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Col sm={3}>
-          <Form.Label>
-            Social Security
-          </Form.Label>
-          </Col>
-          <Col sm="4">
-            <Form.Control defaultValue="###-##-####" />
-          </Col>
-        </Form.Group>
+            <Row>
 
-        <Row>
-
-        <Col sm={3}>
-        <Form.Label>     
-          </Form.Label>
-        </Col>
-        <Col sm="4">
-        <Button id="ExpungeButton" type="submit">
-          Start Expunging
+              <Col sm={3}>
+                <Form.Label>
+                </Form.Label>
+              </Col>
+              <Col sm="4">
+                <Button id="ExpungeButton" type="submit">
+                  Start Expunging
         </Button>
-        </Col>
-        </Row>
-      </Form>
-      
+              </Col>
+            </Row>
+          </Form>
+
         </Col>
         <Col></Col>
       </Row>
-    )
+    </div>
   }
 }
 
