0ad5262c488a724cf4cf377469b088076eb250e9
Ize Imai <izeimai@gmail.com>
2020-06-29 19:42:52 -0400

    set docket_file res.data to docketdata

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -76,6 +76,7 @@ export default function FileUpload() {
                     if (res.status === 200) {
                         setFilePassed(true);
                         setDocketData(res.data);
+                        console.log(docketData.petitioner);
                     }
                 })
                 .catch(err => {
